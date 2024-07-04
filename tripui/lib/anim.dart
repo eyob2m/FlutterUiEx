0ce898f63f9c5505e281faf30cd772dc6b881070
eyob2m <eyob2etemariam@gmail.com>
2024-07-04 18:01:16 +0300

    ui established

---
+++ tripui/lib/anim.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Anim extends StatefulWidget {
+  const Anim({super.key});
+
+  @override
+  State<Anim> createState() => _AnimState();
+}
+
+class _AnimState extends State<Anim> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- tripui/lib/main.dart
+++ tripui/lib/main.dart
@@ -1,115 +1,182 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  PageController _pc = PageController(initialPage: 0);
+  int pgn = 4;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: Scaffold(
+        body: PageView(
+          controller: _pc,
+          children: [
+            Page(
+              1,
+              "assets/images/photo_1_2024-07-03_02-13-15.jpg",
+                "Ethiopia",
+                'Even if it is not your approach for learning, please take a look at echobot.py. It is the de facto base for most of the bots out there. The code for these examples is released to the public domain, so you can start by grabbing the code and building on top of it.',
+                4,
+                378),
+            Page(2,
+            "assets/images/photo_2_2024-07-03_02-13-15.jpg",
+                "Ghana",
+                'Even if it is not your approach for learning, please take a look at echobot.py. It is the de facto base for most of the bots out there. The code for these examples is released to the public domain, so you can start by grabbing the code and building on top of it.',
+                3,
+                78),
+            Page(3,
+            "assets/images/photo_3_2024-07-03_02-13-15.jpg",
+                "Kenya",
+                'Even if it is not your approach for learning, please take a look at echobot.py. It is the de facto base for most of the bots out there. The code for these examples is released to the public domain, so you can start by grabbing the code and building on top of it.',
+                4.5,
+                1020),
+            Page(4,
+            "assets/images/photo_4_2024-07-03_02-13-15.jpg",
+                "Brundi",
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+                'Even if it is not your approach for learning, please take a look at echobot.py. It is the de facto base for most of the bots out there. The code for these examples is released to the public domain, so you can start by grabbing the code and building on top of it.',
+                4.3,
+                789)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget Page(int page,String image,String title, String desc, double rating, int pop) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image:
+                  AssetImage(image),
+              fit: BoxFit.cover)),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                stops: [.4, .8, 1],
+                begin: Alignment.bottomRight,
+                colors: [Colors.black87, Colors.black54, Colors.black26])),
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  page.toString(),
+                  style: TextStyle(color: Colors.white70, fontSize: 30),
+                ),
+                Text(
+                  "/$pgn",
+                  style: TextStyle(color: Colors.white60, fontSize: 20),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 3),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 3),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 3),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 3),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 7),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        rating.toString(),
+                        style: TextStyle(color: Colors.white70),
+                      ),
+                      Text("($pop)",
+                          style: TextStyle(color: Colors.white.withOpacity(.4)))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Text(
+                      desc,
+                      style: TextStyle(
+                          height: 1.4, color: Colors.white.withOpacity(.6)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white12, width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    child: Text(
+                      "Read More",
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(.7),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ]),
+        ),
+      ),
     );
   }
 }
